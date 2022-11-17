3864f29b328c54c221c1e683e38ec06d09bfe734
Tuag <igortuag@gmail.com>
2022-11-17 08:06:57 -0300

    add imports to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { FiArrowUpRight } from "react-icons/fi";
+import { AiOutlineDown } from "react-icons/ai";
+import { HiOutlineDotsVertical } from "react-icons/hi";
+import ethlogo from "../assets/ethlogo.png";
+import uniswapLogo from "../assets/uniswapLogo.png";
+import Image from "next/image";
 
 const Header = () => {
-  return (
-    <header>Header</header>
-  )
-}
+  return <header>Header</header>;
+};
 
-export default Header
+export default Header;
