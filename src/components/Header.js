1a9aca43bcd37d1c12f879c9f18112d3bea9dd2a
Tuag <igortuag@gmail.com>
2022-11-22 08:37:02 -0300

    add headerLogo styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,6 +8,7 @@ import Image from "next/image";
 
 const style = {
   wrapper: "p-4 w-screen flex justify-between items-center",
+  headerLogo: "flex w-1/4 items-center justify-start",
 };
 
 const Header = () => {
