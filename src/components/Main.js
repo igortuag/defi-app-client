1d7d47c9568b02f83ac77e24674d949d34edfc16
Tuag <igortuag@gmail.com>
2022-11-24 08:43:34 -0300

    add imports dependences

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,10 @@
 import React from "react";
+import Image from 'next/Image'
+import { RiSettings3Fill } from "react-icons/ri";
+import { AiOutlineDown } from "react-icons/ai";
+import ethLogo from "../assets/eth.png";
+
+const styles = {}
 
 function Main() {
   return <main>Main</main>;
