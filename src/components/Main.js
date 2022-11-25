bec699cac5da67f0b038bb9091c860a26c51b54e
Tuag <igortuag@gmail.com>
2022-11-25 08:28:07 -0300

    add styles to formHeader

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,12 +7,15 @@ import ethLogo from "../assets/eth.png";
 const styles = {
   wrapper: "w-screen flex items-center justify-center flex-col mt-14",
   content: "bg-[#181B1F] w-[90%] h-[40rem] rounded-2xl p-4",
+  formHeader: "px-2 flex items-center justify-between font-semibold",
 };
 
 function Main() {
   return (
     <main className={styles.wrapper}>
-      <div className={styles.content}></div>
+      <form className={styles.content}>
+        <header className={styles.formHeader}></header>
+      </form>
     </main>
   );
 }
