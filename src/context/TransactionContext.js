c4f9cee3d2f37c70d09b410fe294f1c8e2fabc98
Tuag <igortuag@gmail.com>
2022-12-01 06:46:14 -0300

    add eth data value

--- src/context/TransactionContext.js
+++ src/context/TransactionContext.js
@@ -1,3 +1,9 @@
 import { useState, useEffect } from 'react';
 
 export const TransactionContext = React.createContext();
+
+let eth
+
+if (typeof window !== 'undefined') {
+  eth = window.ethereum
+}
