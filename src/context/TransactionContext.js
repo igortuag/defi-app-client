b89e2e5418301a6711dfe6e60e74eecbedb4242a
Tuag <igortuag@gmail.com>
2022-12-01 06:47:40 -0300

    add alert to not instaled metamask

--- src/context/TransactionContext.js
+++ src/context/TransactionContext.js
@@ -7,3 +7,13 @@ let eth
 if (typeof window !== 'undefined') {
   eth = window.ethereum
 }
+
+const connectWallet = async (metamask = eth) => { 
+  try {
+    if (!metamask) {
+      return alert ('Please install metamask')
+    }
+  } catch (error) {
+    
+  }
+}
