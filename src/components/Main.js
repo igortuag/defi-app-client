5e4698565618e65efeb05964777c3bc328e04376
Tuag <igortuag@gmail.com>
2022-12-14 09:09:45 -0300

    add context content to main

--- src/components/Main.js
+++ src/components/Main.js
@@ -17,10 +17,13 @@ const styles = {
     "flex h-min items-center justify-between w-full bg-[#2D2F36] hover:bg-[#41444F] font-medium cursor-pointer p-2 mt-[-0.2rem] text-xl rounded-2xl",
   currencySelectorTicker: "mx-2",
   currencySelectorArrow: "text-lg",
-  confirmButton: "bg-[#2172E5] my-2 rounded-2xl py-2 text-xl font-semibold flex items-center justify-center cursor-pointer border border[-#2172E5] hover:border-[#234169]",
+  confirmButton:
+    "bg-[#2172E5] my-2 rounded-2xl py-2 text-xl font-semibold flex items-center justify-center cursor-pointer border border[-#2172E5] hover:border-[#234169]",
 };
 
 function Main() {
+  const { formData, setFormData } = React.useContext(TransactionContext);
+
   return (
     <main className={styles.wrapper}>
       <form
